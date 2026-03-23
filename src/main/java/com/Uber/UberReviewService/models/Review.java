51fd9b4005f02326e970a3f54778502eda6697e3
Nasir <nasirhossain1070@gmail.com>
2026-03-23 19:18:20 +0530

    Added basemodel class using mappedSuperclass

--- src/main/java/com/Uber/UberReviewService/models/Review.java
+++ src/main/java/com/Uber/UberReviewService/models/Review.java
@@ -14,28 +14,17 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "bookingreview")
-public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private long id;
+public class Review extends  BaseModel{
+
 
     @Column(nullable = false)
    private String content;
 
    private Double rating;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-   private Date createdAt;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    private  Date updatedAt;
 
     @Override
     public  String toString(){
