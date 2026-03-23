a9d64875d475c001afa383af7c1de342aeed72f3
Nasir <nasirhossain1070@gmail.com>
2026-03-23 16:20:36 +0530

    Added more  columns for reviews table

--- src/main/java/com/Uber/UberReviewService/models/Review.java
+++ src/main/java/com/Uber/UberReviewService/models/Review.java
@@ -1,11 +1,31 @@
 package com.Uber.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
 
 }
